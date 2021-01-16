6b76678c34de18e15828f4790c43ebbd041e0086
bmirisola <bennymirisola@gmail.com>
2021-01-15 20:36:41 -0500

    added error checking

--- tcp_client.c
+++ tcp_client.c
@@ -8,25 +8,28 @@
 
 int main(){
 
-  //create a socket
-  int network_socket;
-  //			domain of socket, type of socket (TCP vs UDP), protocol
-  network_socket = socket(AF_INET, SOCK_STREAM, 0);
+  	//create a socket
+  	int network_socket;
+ 	//		domain of socket, type of socket (TCP vs UDP), protocol
+  	network_socket = socket(AF_INET, SOCK_STREAM, 0);
 
-  //specify am address for the socket
-  struct sockaddr_in server_address;
-  server_address.sin_family = AF_INET
-  server_address.sin_port = htons(9002);
-  //server_address = structure that hold some information about the address
-  //sin_addr = structure containing one field that holds the actual address
-  //s_addr = holds address
-  server_address.sin_addr.s_addr = INADDR_ANY;
+ 	 //specify am address for the socket
+  	struct sockaddr_in server_address;
+  	server_address.sin_family = AF_INET
+  	server_address.sin_port = htons(9002);
+  	
+	//server_address = structure that hold some information about the address
+  	//sin_addr = structure containing one field that holds the actual address
+  	//s_addr = holds address
+ 	 server_address.sin_addr.s_addr = INADDR_ANY;
 
-  //	 actaul socket, pointer to address
-  connect(network_socket, (struct sockaddr *) &server_address, sizeof(server_address)
+  	//	 actaul socket, pointer to address
+  	connect(network_socket, (struct sockaddr *) &server_address, sizeof(server_address)
 
-	  //check for error with the connection
-	  if(connection_status == -1){
+	//check for error with the connection
+	if(connection_status == -1){
+ 		printf("There was an error making a connection to the remote socket \n\n")
+	}
        
 
 
